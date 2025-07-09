dd303c097b79e44ab5cc9a55dc52a982f4c4c250
Eshwar Lal <eshwarlal001288@gmail.com>
2025-07-10 00:10:42 +0500

    initial

---
+++ script.js
@@ -0,0 +1,53 @@
+const terminal = document.getElementById("terminal");
+const proposal = document.getElementById("proposal");
+
+const messages = [
+  ">> Booting up system...",
+  ">> Hacking love.exe...",
+  ">> Accessing feelings_db...",
+  ">> Injecting honesty...",
+  ">> Scanning compatibility...",
+  ">> matchFound = true",
+  ">> Initiating proposal_protocol()...",
+  ">> Loading...",
+  ">> Preparing message for [My Detective]...",
+  "",
+  "Hello My Detective love... 💚",
+  "I have something special to tell you...",
+  "You’ve always been more than just a best friend to me.",
+  "So here it goes..."
+];
+
+let index = 0;
+let charIndex = 0;
+
+function typeLine() {
+  if (index < messages.length) {
+    if (charIndex < messages[index].length) {
+      terminal.innerHTML += messages[index].charAt(charIndex);
+      charIndex++;
+      setTimeout(typeLine, 40);
+    } else {
+      terminal.innerHTML += '\n';
+      index++;
+      charIndex = 0;
+      setTimeout(typeLine, 300);
+    }
+  } else {
+    showProposal();
+  }
+}
+
+function showProposal() {
+  proposal.style.display = "block";
+}
+
+function accept() {
+  alert("💚 She said YES! System upgraded to LOVE MODE 💚");
+}
+
+function decline() {
+  alert("😅 Okay, no worries. System remains BEST FRIEND MODE.");
+}
+
+typeLine();
